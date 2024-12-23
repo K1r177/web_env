e6a6389d741f659a52bd3c02354b16bf710b69de
K1r177 <K1r177@yandex.ru>
2024-12-23 16:49:42 +0300

    lesson 17

---
+++ src/app/Http/Controllers/HomeController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Country;
+use App\Models\Post;
+
+class HomeController extends Controller{
+
+    public function index(){
+        $title = 'Home Page';
+
+        /* $posts = Post::all()->toArray();
+        dump($posts); */
+
+        /* $posts = Post::query()->first()->toArray();
+        dump($posts); */
+
+        /* $posts = Post::query()->find(2, ['id', 'title', 'slug'])->toArray();
+        dump($posts); */
+
+        /* $countries = Country::all(['Code', 'Name', 'Population'])->toArray();
+        dump($countries); */
+
+        /* $countries = Country::query()
+            ->where('Population', '>', 100_000_000)
+            ->orderBy('Population', 'desc')
+            ->limit(5)
+            ->get(['Code', 'Name', 'Population']); */
+        //return $countries->toJson();
+        //return response()->json($countries);
+
+        $country = Country::query()->first();
+        dump($country->toArray());
+
+        //return View('home.index', compact('title'));
+    }
+
+}
