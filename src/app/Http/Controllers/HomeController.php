367255ff50df7edf9fe7938d8bb7377f51598fc2
K1r177 <K1r177@yandex.ru>
2025-01-10 15:36:55 +0300

    lesson 21

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -18,9 +18,39 @@ class HomeController extends Controller{
        /*  $post = Post::query()->where('category_id', '=', 2)->first();
         dump($post->toArray()); */
 
-        $post = Post::query()->find(5);
+        /* $post = Post::query()->find(5);
         dump($post->toArray());
         dump($post->category);
-        dump($post->category->title);
+        dump($post->category->title); */
+
+        /* $category = Category::query()->find(1);
+        dump($category->toArray());
+        $posts = $category->posts;
+        dump($posts->toArray()); */
+
+        /* $post = Post::query()->find(6);
+        dump($post->toArray());
+        dump($post->category->toArray()); */
+
+        /* $categories = Category::with('posts')->get();
+        dump($categories->toArray());
+
+        foreach($categories as $category){
+            echo "{$category->title}<br>";
+            foreach($category->posts as $post){
+                echo "{$post->title}<br>";
+            }
+            echo '<hr>';
+        } */
+
+        /* $categories = Category::query()->withCount('posts')->get();
+        dump($categories);
+        foreach($categories as $category){
+            echo "{$category->title} | ({$category->posts_count})<br>";
+        } */
+
+        $category = Category::query()->find(1);
+        dump($category->posts()->orderBy('id', 'desc')->get()->toArray());
+
     }
 }
--- src/app/Models/Category.php
+++ src/app/Models/Category.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Category extends Model
@@ -11,4 +12,10 @@ class Category extends Model
     {
         return $this->hasOne(Post::class);
     }
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
 }
